6e0610c9391d84b5bed7e2b28aaeb53d714efb6b
Roman Sushchinsky <romsu78@gmail.com>
2022-08-15 23:04:49 +0300

    Add words count feature

--- viceversa/views.py
+++ viceversa/views.py
@@ -5,6 +5,8 @@ def home(request):
 
 def reverse(request):
 	user_text = request.GET['usertext']
+	words = user_text.split()
+	number_of_words = len(words)
 	#print(user_text)
 	reverse_text = user_text[::-1]
-	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':reverse_text})
+	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':reverse_text, 'number_of_words':number_of_words})
